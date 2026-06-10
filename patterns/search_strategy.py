ed50de67f16ce8729a56476b347d368763282cba
V <varunshankar2k6@gmail.com>
2026-06-10 14:49:50 +0530

    Question 12 Strategic pattern

---
+++ patterns/search_strategy.py
@@ -0,0 +1,10 @@
+#QUestion 12 Strategic Planning
+class SkillSearch:
+    def search(self):
+        print("Searching jobs by skill")
+class LocationSearch:
+    def search(self):
+        print("Searching jobs by location")
+class SalarySearch:
+    def search(self):
+        print("Searching jobs by salary")
