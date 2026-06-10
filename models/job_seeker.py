31b6abb29ffa96ac9669ae7149d98ac809243b32
V <varunshankar2k6@gmail.com>
2026-06-10 10:48:03 +0530

    Qn 2 Job seeker

--- models/job_seeker.py
+++ models/job_seeker.py
@@ -1,12 +1,13 @@
+#Inheriting from class user and logger 
 from models.user import User
 from utils.decorators import logger
-
+#Defing class
 class JobSeeker(User):
-
+    #Basic init constructor function
     def __init__(self, name, email, skills):
         super().__init__(name, email)
         self.skills = skills
-
+    #Applying the logger function
     @logger
     def apply_job(self, job_title):
-        print(self.name, "applied for", job_title)
+        print(self.name, "applied for the position ", job_title)
