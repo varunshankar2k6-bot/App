0fbb2fcd669a358b9c63a860fb053c898f0dca1d
V <varunshankar2k6@gmail.com>
2026-06-10 10:52:34 +0530

    Qn 4 Premium job Seeker

--- models/premium_job_seeker.py
+++ models/premium_job_seeker.py
@@ -1,10 +1,11 @@
+#Inheriting from Job seeker class
 from models.job_seeker import JobSeeker
-
+#Defing the class
 class PremiumJobSeeker(JobSeeker):
-
+    #Defing the init contructor with addition membership type
     def __init__(self, name, email, skills, membership_type):
         super().__init__(name, email, skills)
         self.membership_type = membership_type
-
+    #Defining function priority
     def priority_apply(self):
         print("Priority application enabled")
